b137a601d9940465b614f1a85136c78f113a63db
arefin5 <arefintalukder5@gmail.com>
2022-10-31 01:32:44 +0600

    object and arrey compare

--- arrey.js
+++ arrey.js
@@ -7,3 +7,13 @@ arr.pop()
 arr.shift(2)
 arr.unshift(222)
 console.log(arr)
+
+// finding element fron an   arrey
+let arr2=['a','b','c','d']
+for (elm of arr2){
+    if(elm==='c'){
+        console.log(true)
+    }
+    // if(elm[0]==='a')console.log(elm)
+}
+// forEach,map ,filter ,reduce, big O(n)
